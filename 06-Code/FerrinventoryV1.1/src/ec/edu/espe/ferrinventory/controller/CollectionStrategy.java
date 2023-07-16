b3824ab14557b9450beb2e360f626ae8d8e011b8
juan784 <juanse.rueda1@gmail.com>
2023-07-16 00:13:37 -0500

    default

---
+++ 06-Code/FerrinventoryV1.1/src/ec/edu/espe/ferrinventory/controller/CollectionStrategy.java
@@ -0,0 +1,12 @@
+
+package ec.edu.espe.ferrinventory.controller;
+
+/**
+ *
+ * @author Alejandro Andrade, Scriptal, DCCO_ESPE
+ */
+public interface CollectionStrategy {
+    
+    public boolean obtainUser(int id, String password);
+    
+}
